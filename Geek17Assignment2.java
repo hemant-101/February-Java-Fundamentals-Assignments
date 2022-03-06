f5e96976b1217ab1686ecce8001aee928977587e
hemant-101 <81548528+hemant-101@users.noreply.github.com>
2022-03-06 13:32:58 +0530

    Add files via upload

---
+++ Geek17Assignment2.java
@@ -0,0 +1,13 @@
+package geek17febAssign;
+
+public class Geek17Assignment2 {
+    public static void main(String[] args) {
+        int n = 4;
+
+        if(n>10){
+            System.out.println("Given number is greater than 10");
+        }else{
+            System.out.println("Given number is lesser than 10");
+        }
+    }
+}
